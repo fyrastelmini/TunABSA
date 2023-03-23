41671aef99bb9b9e8829e2e4eee2254b47ed2107
mtelmini <mohamed.telmini@ensae.fr>
2023-03-23 08:36:39 +0000

    fixed annotation

--- annotation/annotate.py
+++ annotation/annotate.py
@@ -5,11 +5,22 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-df_filtered=pd.read_csv("dataset.csv")
+df_filtered=pd.read_csv("sample.csv")
 df_filtered['word_list'] = df_filtered['word_list'].apply(ast.literal_eval)
 def clear_console():
     sys.stdout.write("\033[2J\033[1;1H")
     sys.stdout.flush()
+clear_console()
+print("welcome to the annotation script\n")
+print("this script is made to annotate files structured like this one\n")
+print(df_filtered,"\n")
+print("the script will go through every word and ask you for an input according to its label\n")
+print("if a word doesnt correspond to any label, press [enter] directly, you can press 3 to skip the sentence and not save it in the outpit dataset\n")
+print("the output file will be named dataset_out.csv and will be updated after each sentence added\n")
+print("if a file named dataset_out.csv already exists, the annotation will continue from the last input dataset sentence that is not in it\n")
+print("press [enter]")
+input()
+
 for count,i,j,lab in zip(range(df_filtered.shape[0]),df_filtered["sentence"],df_filtered["word_list"],df_filtered["label"]):
     clear_console()
     labels_subject=[]
